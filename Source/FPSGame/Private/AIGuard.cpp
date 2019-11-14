b11b91b38b633b52f2b347ef07ab131676135011
SaviorKai <55675002+SaviorKai@users.noreply.github.com>
2019-11-14 11:01:22 +1100

    25. Setup AI Sight Sense
    Occlusion = Something that is in the way.
    - Hearing threshhold = 800;     ///Hearing regardless of Occlusion
    - LOSHearing Threshhold = 1500   /// Lind of sight based hearing.
    - Sight Radius = 3000 and Peripheral Angle = 30    ///How far and how wide he can see.
    
    Remember: This component has it's own events. Register one of the events in C++ code to use it.
    - Registered the "OnPawnSeen()" delegate, and bound it to: HandleOnPawnSeen(APawn* SeenPawn)
    
    Used a debug sphere to draw the position of the seen character, evertime it's spotted:
    - #include "DrawDebugHelpers.h" //DrawDebugSphere()
    - DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Blue, false, 10.0f);

--- Source/FPSGame/Private/AIGuard.cpp
+++ Source/FPSGame/Private/AIGuard.cpp
@@ -3,6 +3,7 @@
 
 #include "AIGuard.h"
 #include "Perception/PawnSensingComponent.h" //UPawnSensingComponent
+#include "DrawDebugHelpers.h" //DrawDebugSphere()
 
 // Sets default values
 AAIGuard::AAIGuard()
@@ -10,6 +11,9 @@ AAIGuard::AAIGuard()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComponent"));   ///Note: this doesn't need to be attached to anything.
+
+	//Bind the 'OnSeenPawn()' event to our 'HandleOnPawnSeen()' function.
+	PawnSensingComp->OnSeePawn.AddDynamic(this, &AAIGuard::HandleOnPawnSeen);
 	
 
 }
@@ -26,4 +30,13 @@ void AAIGuard::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AAIGuard::HandleOnPawnSeen(APawn* SeenPawn)
+{
+	if (!SeenPawn) { return; }																				//Pointer Protection
+	
+	//Debug Code
+	UE_LOG(LogTemp, Warning, TEXT("I See you!"));
+	DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Blue, false, 10.0f);		
 }
--- Source/FPSGame/Public/AIGuard.h
+++ Source/FPSGame/Public/AIGuard.h
@@ -21,11 +21,15 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleAnywhere, Category"Components")
+	UPROPERTY(VisibleAnywhere, Category="Components")
 		UPawnSensingComponent* PawnSensingComp;
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	//Create the function that will handle the pawn seen delegate, and use the signature of "OnSeePawn()" from the UE4 engine.
+	UFUNCTION() //ALWAYS HAS TO BE HERE FOR DELEGATE BINDING!
+		void HandleOnPawnSeen(APawn* SeenPawn); 
 
 };
